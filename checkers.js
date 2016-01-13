965c2d782331c423d82edd4e4e6a57f1394581dd
Ethan D <erdesi90562@gmail.com>
2016-01-13 13:47:03 -0500

    basic setUpRed functionality

--- checkers.js
+++ checkers.js
@@ -1,5 +1,3 @@
-// In this exercise, we'll be laying the groundwork for a Checkers game. Your task is to implement two functions:
-
 // setSquare, which places a player (either 'R' or 'B') at a particular row and column on the board.
 // getPieceAt, which returns the piece at a particular row and column; if there's no piece at that position, it should return null.
 var checkerboard = [[null, null, null, null, null, null, null, null],
@@ -10,6 +8,9 @@ var checkerboard = [[null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null]];
+
+var player1 = 'R';
+var player2 = 'B';
 
 var setSquare = function(player, row, col) {
   // Your Code Here
@@ -31,5 +32,18 @@ function clearBoard() {
 		for (var z = boardCols; z >= 0; z--) {
 			checkerboard[i][z] = null;
 		}
+	}
+}
+
+function setUpRed() {
+	var boardRows = checkerboard.length-1;
+	var boardCols = checkerboard[0].length-1;
+
+	for (var i = 0; i <= boardCols; i+=2) {
+			checkerboard[0][i] = player1;
+			checkerboard[2][i] = player1;
+	}
+	for (var z = 1; z <= boardCols; z+=2) {
+			checkerboard[1][i] = player1;
 	}
 }
