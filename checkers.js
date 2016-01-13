98cd37e6d01c18bb030df4355ea784a233e6817e
Ethan D <erdesi90562@gmail.com>
2016-01-13 11:09:02 -0500

    basic setSquare functionality

--- checkers.js
+++ checkers.js
@@ -1,3 +1,7 @@
+// In this exercise, we'll be laying the groundwork for a Checkers game. Your task is to implement two functions:
+
+// setSquare, which places a player (either 'R' or 'B') at a particular row and column on the board.
+// getPieceAt, which returns the piece at a particular row and column; if there's no piece at that position, it should return null.
 var checkerboard = [[null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null],
                     [null, null, null, null, null, null, null, null],
@@ -9,14 +13,9 @@ var checkerboard = [[null, null, null, null, null, null, null, null],
 
 var setSquare = function(player, row, col) {
   // Your Code Here
+  checkerboard[row][col] = player;
 }
 
 var getPieceAt = function(row, col) {
   // Your Code Here
 }
-
-// In this exercise, we'll be laying the groundwork for a Checkers game. Your task is to implement two functions:
-
-// setSquare, which places a player (either 'R' or 'B') at a particular row and column on the board.
-// getPieceAt, which returns the piece at a particular row and column; if there's no piece at that position, it should return null.
-// Be sure to save your repl.it session - we'll be using it in a later exercise.
