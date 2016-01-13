5c43e4147376bf465fa30eda27338c780d46dca7
Ethan D <erdesi90562@gmail.com>
2016-01-13 10:30:29 -0500

    Blank groundWork

---
+++ checkers.js
@@ -0,0 +1,22 @@
+var checkerboard = [[null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null],
+                    [null, null, null, null, null, null, null, null]];
+
+var setSquare = function(player, row, col) {
+  // Your Code Here
+}
+
+var getPieceAt = function(row, col) {
+  // Your Code Here
+}
+
+// In this exercise, we'll be laying the groundwork for a Checkers game. Your task is to implement two functions:
+
+// setSquare, which places a player (either 'R' or 'B') at a particular row and column on the board.
+// getPieceAt, which returns the piece at a particular row and column; if there's no piece at that position, it should return null.
+// Be sure to save your repl.it session - we'll be using it in a later exercise.
